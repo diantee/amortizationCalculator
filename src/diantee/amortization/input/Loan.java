8ede6333a062d0d8449f6a279c3994768a52e3eb
Diante <diante.edwards1@gmail.com>
2018-04-20 01:03:16 -0400

    Renamed Loan class variables

--- src/diantee/amortization/input/Loan.java
+++ src/diantee/amortization/input/Loan.java
@@ -1,19 +1,19 @@
 package diantee.amortization.input;
 
 public class Loan{
-    protected double initialPrincipal;
-    protected int yrs;
-    protected double irate;
-    protected double prepay;
-    protected double currentPrincipal;
-    protected int currentYr = 1;
-    protected double totalPmt;
+    private double initialPrincipal;
+    private int yrs;
+    private double iRate;
+    private double prePaymentRate;
+    private double currentPrincipal;
+    private int currentYr = 1;
+    private double totalPmt;
 
-    public Loan(double principal, int yrs, double irate, double prepay) {
+    public Loan(double principal, int yrs, double irate, double prePaymentRate) {
         this.initialPrincipal = principal;
         this.yrs = yrs;
-        this.irate = irate;
-        this.prepay = prepay;
+        this.iRate = irate;
+        this.prePaymentRate = prePaymentRate;
         currentPrincipal = initialPrincipal;
         totalPmt = currentPrincipal * ((irate * Math.pow((1 + irate),yrs) /(Math.pow((1 + irate),yrs)-1)));
 
@@ -22,27 +22,27 @@ public class Loan{
 
 
     public void Amortization() {
-        if(prepay > 0) {
+        if(prePaymentRate > 0) {
             System.out.println("============ Year " + currentYr + " ================");
             System.out.printf("Beginning Balance: $%.2f\n", currentPrincipal);
-            double iratePmt = currentPrincipal * irate;
+            double iratePmt = currentPrincipal * iRate;
             double principalPmt = totalPmt - iratePmt;
             System.out.printf("Total Payment: $%.2f\n", totalPmt);
             System.out.printf("Interest rate payment: %.2f\n", iratePmt);
             System.out.printf("Principal payment: $%.2f\n", principalPmt);
             currentPrincipal -= principalPmt;
             System.out.printf("Balance Before Prepayment: $%.2f\n", currentPrincipal);
-            double prepayAmt = (prepay*initialPrincipal);
+            double prepayAmt = (prePaymentRate*initialPrincipal);
             System.out.printf("Subtracting Prepayment: $%.2f\n\n", prepayAmt);
             currentPrincipal -= prepayAmt;
             System.out.printf("Ending Balance: $%.2f\n\n", currentPrincipal);
-            totalPmt = currentPrincipal * ((irate * Math.pow((1 + irate),yrs-1) /(Math.pow((1 + irate),yrs-1)-1)));
+            totalPmt = currentPrincipal * ((iRate * Math.pow((1 + iRate),yrs-1) /(Math.pow((1 + iRate),yrs-1)-1)));
             currentYr++;
         }
         for(int i = currentYr; i <= yrs; i++) {
             System.out.println("============ Year " + currentYr + " ================");
             System.out.printf("Beginning Balance: $%.2f\n", currentPrincipal);
-            double iratePmt = currentPrincipal * irate;
+            double iratePmt = currentPrincipal * iRate;
             double principalPmt = totalPmt - iratePmt;
             System.out.printf("Total Payment: $%.2f\n", totalPmt);
             System.out.printf("Interest rate payment: $%.2f\n", iratePmt);
